beef5cfc423b2257d163d954100b9d1cee92f376
paktusov <paktusov@gmail.com>
2025-11-14 11:56:53 +0000

    Fix issues

--- community_edition/app.py
+++ community_edition/app.py
@@ -6,11 +6,9 @@ from community_edition.services.authentication import redirect_to_login
 from community_edition.services.env import load_env
 
 
-env = load_env()
-
-
 def create_app() -> Flask:
     app = Flask(__name__)
+    env = load_env()
     app.secret_key = env.get("FLASK_SECRET_KEY") or "finmars-secret-key"
     app.register_blueprint(configurate)
     app.register_blueprint(authentication)
@@ -22,6 +20,7 @@ def create_app() -> Flask:
         if request.endpoint in allowlisted_endpoints or request.endpoint is None:
             return
 
+        env = load_env()
         auth_login = env.get("ADMIN_USERNAME")
         auth_password = env.get("ADMIN_PASSWORD")
         if not auth_login or not auth_password:
